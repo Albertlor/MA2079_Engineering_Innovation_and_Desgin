b65a5484b17dfa3abe6c8d110d0da4e5cf8a2238
Albert <wensinlor@gmail.com>
2023-03-12 01:44:20 +0800

    Pose Estimation feature accomplished
    Signed-off-by: Albert <wensinlor@gmail.com>

--- pose_estimation/main.py
+++ pose_estimation/main.py
@@ -2,18 +2,12 @@
 
 import cv2
 import time
-import mediapipe as mp
 import json
+import numpy as np
 
-from body_info.keypoints import detectPose
-from utils import calculateAngle
+from utils import magnitude
+from body_info.keypoints import Pose
     
-
-# Initializing mediapipe pose class.
-mp_pose = mp.solutions.pose
-
-# Setup Pose function for video.
-pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, model_complexity=1)
 
 # Initialize the VideoCapture object to read from the webcam.
 video = cv2.VideoCapture(0)
@@ -23,6 +17,8 @@ video.set(4,960)
 # Initialize a resizable window.
 cv2.namedWindow('Frames', cv2.WINDOW_NORMAL)
 count_frame = 0
+count = 0
+g_dir = 0
 replay = 0
 last_time = time.time()
 
@@ -66,9 +62,6 @@ while True:
     if ret:
         #frame = imutils.resize(frame, width=800, inter=cv2.INTER_LINEAR)
         cv2.putText(frame, "fps: " + fps, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 1, cv2.LINE_AA)
-
-        # # Flip the frame horizontally for natural (selfie-view) visualization.
-        # frame = cv2.flip(frame, 1)
         
         # Get the width and height of the frame
         frame_height, frame_width, _ =  frame.shape
@@ -76,32 +69,50 @@ while True:
         # Resize the frame while keeping the aspect ratio.
         frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
         
+        pose = Pose(count_frame, frame, display=False)
+        
         # Perform Pose landmark detection.
-        frame, landmarks, real_landmarks = detectPose(count_frame, frame, pose_video, display=False)
+        frame, landmarks, real_landmarks, confidence_index, front = pose.detectPose()
+
+        if front:
+            cv2.putText(frame, "Person is facing towards the robot, do a 90deg turn", (50, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+        else:
+            cv2.putText(frame, "Stay in this manner", (50, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
         
         try:
-            if landmarks:
-                
-                # # Perform the Pose Classification.
-                # frame, _ = classifyPose(landmarks, frame, display=False)
-                with open('./database/shoulder.json') as f1:
-                    config1 = json.load(f1)
+            if front == None:
+                cv2.putText(frame, f"Region of Interest is not detected", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+            if front == 0:
+                if confidence_index==2:
+                    
+                    # # Perform the Pose Classification.
+                    # frame, _ = classifyPose(landmarks, frame, display=False)
+                    with open('./database/shoulder.json') as f1:
+                        config1 = json.load(f1)
 
-                shoulder = config1[f'SHOULDER_{count_frame}']
+                    shoulder = config1[f'SHOULDER_{count_frame}']
 
-                with open('./database/hip.json') as f2:
-                    config2 = json.load(f2)
+                    with open('./database/hip.json') as f2:
+                        config2 = json.load(f2)
 
-                hip = config2[f'HIP_{count_frame}']
+                    hip = config2[f'HIP_{count_frame}']
 
-                with open('./database/knee.json') as f3:
-                    config3 = json.load(f3)
+                    with open('./database/knee.json') as f3:
+                        config3 = json.load(f3)
 
-                knee = config3[f'KNEE_{count_frame}']
+                    knee = config3[f'KNEE_{count_frame}']
 
-                low_back_angle = round(abs(calculateAngle(shoulder, hip, knee) - 180), 3)
+                    if count == 0:
+                        r = np.array(shoulder) - np.array(hip)
+                        g_dir = np.multiply(r, 1/(magnitude(r)))
+                        count += 1
 
-                cv2.putText(frame, f"Spine Angle: {low_back_angle}", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 1, cv2.LINE_AA)
+                    low_back_angle = round(abs(pose.calculateAngle(g_dir, shoulder, hip, knee)), 3)
+
+                    cv2.putText(frame, f"Spine Angle: {low_back_angle}", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 1, cv2.LINE_AA)
+
+                else:
+                    cv2.putText(frame, f"Region of Interest is not detected", (50, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
 
             cv2.imshow("Frames", frame)
 
